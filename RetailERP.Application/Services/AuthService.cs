85406b9d3698d3edc1e5a264f6034061e3ead7c5
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-12-07 12:44:13 +0600

    running

--- RetailERP.Application/Services/AuthService.cs
+++ RetailERP.Application/Services/AuthService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using RetailERP.Application.DTOs.Auth;
 using RetailERP.Application.Interfaces;
---
+++ RetailERP.Web/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RetailERP.Domain.Constants;
+using RetailERP.Domain.Entities;
+
+namespace RetailERP.Web.Controllers;
+
+[Authorize(Roles = RoleConstants.AdminAndManager)]
+public class UserController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    // List Users
+    public async Task<IActionResult> Index()
+    {
+        var users = _userManager.Users;
+        return View(await users.ToListAsync());
+    }
+}
